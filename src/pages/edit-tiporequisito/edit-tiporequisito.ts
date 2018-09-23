0a550d965bdfe4a9c5c3e7e38096c29868b36088
Karine <karinefernandes1994@gmail.com>
2018-09-23 17:32:36 -0300

    modificação dos metodos alterar e excluir

--- src/pages/edit-tiporequisito/edit-tiporequisito.ts
+++ src/pages/edit-tiporequisito/edit-tiporequisito.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ListTiporequisitoPage } from '../list-tiporequisito/list-tiporequisito'
-import {HttpClient} from '@angular/common/http';
-import {TipoRequisito} from "../../modelo/TipoRequisito";
+import { ListTiporequisitoPage } from '../list-tiporequisito/list-tiporequisito';
+import { HttpClient } from '@angular/common/http';
+import { TipoRequisito } from "../../modelo/TipoRequisito";
+
 
 @IonicPage()
 @Component({
@@ -14,12 +15,13 @@ export class EditTiporequisitoPage {
   
   codReq: number;
   nomeReq: String;
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private _http:HttpClient) {
     this.codReq = navParams.get('codtipreq');
-    let requisitos = this.listaRequisitos;
-    this.nomeReq=this.navParams.get('nomtipreq');
+    //let requisitos = this.listaRequisitos;
+    this.nomeReq = this.navParams.get('nomtipreq');
   }
 
   ionViewDidLoad() {
@@ -27,7 +29,12 @@ export class EditTiporequisitoPage {
   }
 
   alterar(){
-    this._http.put('/api/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/codReq',{})
+    let requisito = {
+      "codigo": this.codReq,
+      "nome": this.nomeReq
+    };
+
+    this._http.put('/api/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/codReq',requisito)
     .subscribe(
       (tiporequisito)=>{
         console.log(tiporequisito);
@@ -36,5 +43,17 @@ export class EditTiporequisitoPage {
     
   }
 
-  excluir(){}
+  excluir(){
+    let requisito = {
+      "codigo": this.codReq,
+      "nome": this.nomeReq
+    };
+    this._http.delete('/api/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/'+ requisito)
+    .subscribe(
+      (tiporequisito)=>{
+        console.log(tiporequisito);
+      }
+    );
+
+  }
 }
